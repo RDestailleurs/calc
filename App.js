bd2d8b504e5eb0f28bc619d8b58ac6e85bfa9d1a
RDestailleurs <romain.destailleurs1@gmail.com>
2024-04-25 00:06:10 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function App() {
+  let value = 0;
+const [result, setResult] = setState();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{value}</Text>
+      <Text>{result}</Text>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={}><Text>1</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>2</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>3</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>-</Text></TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={}><Text>4</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>5</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>6</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>+</Text></TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={}><Text>7</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>8</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>9</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>*</Text></TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity onPress={}><Text>0</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>C</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>=</Text></TouchableOpacity>
+        <TouchableOpacity onPress={}><Text>/</Text></TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
