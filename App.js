11d616f948a7112b7b9d3f9200604323e32ad319
RDestailleurs <romain.destailleurs1@gmail.com>
2024-04-25 22:58:33 +0200

    Push pour l'ecf: 3 problèmes restants à régler

--- App.js
+++ App.js
@@ -2,42 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useEffect, useState } from "react";
 export default function App(addValue) {
-  const [value,setValue] = useState();
-  
+  const [value,setValue] = useState(init);
+  const [result, setResult] = useState(init);
+  const init = 0;
   async function addValue(arg){
     setValue(arg);
   }
+  async function operate(){
+    setResult(eval(value));
+    setValue(init);
+  }
   useEffect( () => {
-    addValue()
-  })
+    setResult();
+    addValue();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>{value}</Text>
-      <Text>0</Text>
-      <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity style={styles.num} onPress={addValue({value:1})}><Text>1</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={addValue({value:2})}><Text>2</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={addValue({value:3})}><Text>3</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={addValue({value:"-"})}><Text>-</Text></TouchableOpacity>
+      <Text style={styles.text}>{value}</Text>
+      <Text style={styles.text}>{result}</Text>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"1")}><Text style={styles.text}>1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"2")}><Text style={styles.text}>2</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"3")}><Text style={styles.text}>3</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => addValue(value+"-")}><Text style={styles.text}>-</Text></TouchableOpacity>
       </View>
-      {/* <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity style={styles.num} onPress={value = 4}><Text>4</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={value = 5}><Text>5</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={value = 6}><Text>6</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={value = "+"}><Text>+</Text></TouchableOpacity>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"4")}><Text style={styles.text}>4</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"5")}><Text style={styles.text}>5</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"6")}><Text style={styles.text}>6</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => addValue(value+"+")}><Text style={styles.text}>+</Text></TouchableOpacity>
       </View>
-      <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity style={styles.num} onPress={value = 7}><Text>7</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={value = 8}><Text>8</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.num} onPress={value = 9}><Text>9</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={value = "*"}><Text>*</Text></TouchableOpacity>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"7")}><Text style={styles.text}>7</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"8")}><Text style={styles.text}>8</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"9")}><Text style={styles.text}>9</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => addValue(value+"*")}><Text style={styles.text}>*</Text></TouchableOpacity>
       </View>
-      <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity style={styles.num} onPress={value = 0}><Text>0</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={value = 0}><Text>C</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={value = "="}><Text>=</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.op} onPress={value = "/"}><Text>/</Text></TouchableOpacity>
-      </View> */}
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.num} onPress={() => addValue(value+"0")}><Text style={styles.text}>0</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => addValue(init)}><Text style={styles.text}>C</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => operate()}><Text style={styles.text}>=</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.op} onPress={() => addValue(value+"/")}><Text style={styles.text}>/</Text></TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -49,11 +55,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 20,
   },
   num:{
     backgroundColor: "grey",
+    
   },
   op:{
     backgroundColor: "orange",
   },
+  row:{
+    flexDirection: "row",
+    
+  },
+  text:{
+    fontSize: 40,
+  }
 });
